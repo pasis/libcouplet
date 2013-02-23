08393d5547aefd4d58a3728b10089834e06d2715
Dmitry Podgorny <pasis.ua@gmail.com>
2013-02-24 01:02:21 +0200

    cleanup bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-ACFLAGS=""
-if [ -d /usr/local/share/aclocal ]; then
-    ACFLAGS="-I /usr/local/share/aclocal"
-fi
-
-libtoolize
-aclocal ${ACFLAGS}
-automake --add-missing --foreign --copy
-autoconf
+autoreconf -i

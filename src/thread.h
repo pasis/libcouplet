1798cd751d9adaf4a9ca97de565845be5156cb8d
Dmitry Podgorny <pasis.ua@gmail.com>
2013-02-24 04:27:45 +0200

    added xmpp_sem_timedwait to thread API

--- src/thread.h
+++ src/thread.h
@@ -35,6 +35,7 @@ typedef void *(*thread_func_t) (void *);
 xmpp_sem_t *xmpp_sem_create(const xmpp_ctx_t *ctx, unsigned int value);
 void xmpp_sem_wait(xmpp_sem_t *sem);
 int xmpp_sem_trywait(xmpp_sem_t *sem);
+int xmpp_sem_timedwait(xmpp_sem_t *sem, uint64_t ns);
 void xmpp_sem_post(xmpp_sem_t *sem);
 int xmpp_sem_destroy(xmpp_sem_t *sem);
 

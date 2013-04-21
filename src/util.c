fa502bcf722b87aac36289024387597c93900ce8
Dmitry Podgorny <pasis.ua@gmail.com>
2013-04-21 21:39:54 +0300

    let compiler inline time_elapsed

--- src/util.c
+++ src/util.c
@@ -79,21 +79,6 @@ uint64_t time_stamp(void)
 #endif
 }
 
-/** Get the time elapsed between two time stamps.
- *  This function returns the time elapsed between t1 and t2 by subtracting
- *  t1 from t2.  If t2 happened before t1, the result will be negative.  This
- *  function is used internally by the event loop and timed handlers.
- *
- *  @param t1 first time stamp
- *  @param t2 second time stamp
- *
- *  @return number of milliseconds between the stamps
- */
-uint64_t time_elapsed(uint64_t t1, uint64_t t2)
-{
-    return (uint64_t)(t2 - t1);
-}
-
 /** Disconnect the stream with a memory error.
  *  This is a convenience function used internally by various parts of
  *  the Strophe library for terminating the connection because of a
--- src/util.h
+++ src/util.h
@@ -27,6 +27,22 @@
 
 /* timing functions */
 uint64_t time_stamp(void);
-uint64_t time_elapsed(uint64_t t1, uint64_t t2);
+
+/* inline functions */
+
+/** Get the time elapsed between two time stamps.
+ *  This function returns the time elapsed between t1 and t2 by subtracting
+ *  t1 from t2.  If t2 happened before t1, the result will be negative.  This
+ *  function is used internally by the event loop and timed handlers.
+ *
+ *  @param t1 first time stamp
+ *  @param t2 second time stamp
+ *
+ *  @return number of milliseconds between the stamps
+ */
+inline static uint64_t time_elapsed(uint64_t t1, uint64_t t2)
+{
+    return (uint64_t)(t2 - t1);
+}
 
 #endif /* __LIBSTROPHE_UTIL_H__ */
